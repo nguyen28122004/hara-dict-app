47c2bdf63b1234f0e05a60a14f43caec38d2437d
nguyen28122004 <nguyen28122004@gmail.com>
2023-11-07 15:18:52 +0700

    add the ui of adding meaning function

--- src/com/harafx/Controllers/addController.java
+++ src/com/harafx/Controllers/addController.java
@@ -2,11 +2,18 @@ package com.harafx.Controllers;
 
 import com.harafx.Models.TransferedData;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.DataTruncation;
 import java.util.ResourceBundle;
 
 import com.harafx.Models.Dictionary;
@@ -14,9 +21,35 @@ import com.harafx.Models.Dictionary;
 public class addController implements Initializable {
 
     @FXML
+    VBox meaningWrapPane = new VBox();
+
+    void addMeaningPane() throws IOException {
+        Node node = FXMLLoader.load(getClass().getResource("../view/search-add-meaning.fxml"));
+        meaningWrapPane.getChildren().add(node);
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            addMeaningPane();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        TransferedData.obsMeaningCount.addListener(new ChangeListener<Number>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+                try {
+                    addMeaningPane();
+
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+        });
 
     }
 }
--- src/com/harafx/Models/transferedData.java
+++ src/com/harafx/Models/transferedData.java
@@ -1,8 +1,20 @@
 package com.harafx.Models;
 
+import javafx.beans.Observable;
+import javafx.beans.binding.NumberBinding;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ObservableIntegerValue;
+import javafx.beans.value.ObservableValue;
+import javafx.beans.value.ObservableValueBase;
+
 public class TransferedData {
     public static Dictionary dict;
-    public static int wordIndex = -1;
     public static Defination def;
     public static Meaning meaning;
+    public static int wordIndex = -1;
+    public static int meaningCount = 1;
+    public static IntegerProperty obsMeaningCount = new SimpleIntegerProperty(meaningCount);
 }

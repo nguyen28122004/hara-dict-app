8ca32681a6f6fb0807032e71afb2a49cd74ac527
nguyen28122004 <nguyen28122004@gmail.com>
2023-11-15 18:14:58 +0700

    add google translator

---
+++ src/com/harafx/Controllers/translatorController.java
@@ -0,0 +1,94 @@
+package com.harafx.Controllers;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.harafx.Models.TranslateAPI;
+
+import java.awt.*;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+public class translatorController extends TranslateAPI implements Initializable {
+
+    String[] langs = { "Vietnamese", "English", "Germany", "French", "Russian", "Chinese", "Japanese" };
+    String[] langIso = { "vi", "en", "de", "fr", "ru", "zh", "ja" };
+
+    @FXML
+    TextArea targetArea = new TextArea();
+    @FXML
+    TextArea resultArea = new TextArea();
+    @FXML
+    Button translateButton = new Button();
+
+    @FXML
+    ComboBox<String> langFrom = new ComboBox<>();
+    @FXML
+    ComboBox<String> langTo = new ComboBox<>();
+
+    @FXML
+    Hyperlink hypLink = new Hyperlink();
+
+    void initButtonControl() {
+        addTranslateListener();
+
+        hypLink.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            try {
+                Desktop.getDesktop().browse(new URI("https://translate.google.com/"));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        });
+
+        langFrom.getItems().setAll(langs);
+        langTo.getItems().setAll(langs);
+
+        langFrom.getSelectionModel().selectFirst();
+        langTo.getSelectionModel().selectFirst();
+    }
+
+    String getTranslateRes() throws IOException {
+        int indexFrom = langFrom.getSelectionModel().getSelectedIndex();
+        int indexTo = langTo.getSelectionModel().getSelectedIndex();
+
+        if (indexFrom < 0 || indexTo < 0 || indexFrom == indexTo) {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setHeaderText("Please choose a pair of 2 diffirent langueges");
+            alert.showAndWait();
+            return "";
+        }
+
+        return translate(langIso[indexFrom], langIso[indexTo], targetArea.getText());
+    }
+
+    void addTranslateListener() {
+        translateButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            try {
+                String res = new String();
+                res = getTranslateRes();
+                resultArea.setText(res);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initButtonControl();
+    }
+
+}
---
+++ src/com/harafx/Models/TranslateAPI.java
@@ -0,0 +1,29 @@
+package com.harafx.Models;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class TranslateAPI {
+    public static String translate(String langFrom, String langTo, String text) throws IOException {
+        String urlStr = "https://script.google.com/macros/s/AKfycbxbWZXdP2e-IbKp930C-d1xUwOOEL8o2YwTP8iJqvAUcu2m_PWK7rcROSoDjybv9r0x/exec"
+                +
+                "?q=" + URLEncoder.encode(text, "UTF-8") +
+                "&target=" + langTo +
+                "&source=" + langFrom;
+        URL url = new URL(urlStr);
+        StringBuilder response = new StringBuilder();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+}
